c36a4477798ca58f22faa2d99c151b6d09a7ae51
k0litha <kolithasenevirathne@gmail.com>
2022-06-24 13:18:16 +0530

    Added About me and skills sections

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar';
 import Main from '../components/Main';
+import About from '../components/About';
+import Skills from '../components/Skills';
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Main />
+      <About />
+      <Skills />
       <Component {...pageProps} />
     </>
   );
