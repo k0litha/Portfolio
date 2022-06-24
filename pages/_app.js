b1f658fc53e8b3524a1df09e114f85a8808e6ece
k0litha <kolithasenevirathne@gmail.com>
2022-06-24 13:26:37 +0530

    Projects and contact me

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,19 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar';
 import Main from '../components/Main';
 import About from '../components/About';
+import Projects from '../components/Projects';
 import Skills from '../components/Skills';
+import Contact from '../components/Contact';
 function MyApp({ Component, pageProps }) {
+
   return (
     <>
       <Navbar />
       <Main />
       <About />
       <Skills />
+      <Projects />
+      <Contact />
       <Component {...pageProps} />
     </>
   );
